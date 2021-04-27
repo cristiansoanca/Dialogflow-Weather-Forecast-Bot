583e96d538fa5e008362d56143d565129a5e44ed
cristiansoanca <36848727+cristiansoanca@users.noreply.github.com>
2021-04-27 16:31:01 +0300

    Update

--- webhook.py
+++ webhook.py
@@ -1,43 +1,6 @@
-#import requests
-
 from flask import Flask
-from flask import request
-from flask import jsonify
-
-
-# Flask app should start in global layout
 app = Flask(__name__)
 
-
-# def makeResponse(dialogflow_data):
-#     result = dialogflow_data.get('queryResult')
-#     parameters = result.get('parameters')
-#     city = parameters.get('geo-city')
-#     date = parameters.get('date-time').split('T')[0]
-
-#     # Weather API
-#     r = requests.get(f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid=2cbe6a057de3aadd1d2353cf765a1895")
-#     json_object = r.json()
-#     weather = json_object['list']
-#     for i in range(len(weather)):
-#         if date in weather[i]['dt_txt']:
-#             condition = weather[i]['weather'][0]['description']
-#             break
-
-#     speech = f"The forecast for {city} on {date} is {condition}"
-
-#     return {'fulfillmentText': speech}
-
- 
-#@app.route('/webhook', methods=['POST'])
-#def webhook():
-    #dialogflow_data = request.get_json(silent=True)
-    #return jsonify(makeResponse(dialogflow_data))
-
-@app.route('/', methods=['GET'])
+@app.route("/")
 def hello():
-    return 'hello world!'
-
-if __name__ == '__main__':
-    app.run(port=443)
-
+    return "Hello, World!"
