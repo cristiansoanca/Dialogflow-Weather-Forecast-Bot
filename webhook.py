49b2a055f35151878689109ab07dd49f32288e70
cristiansoanca <36848727+cristiansoanca@users.noreply.github.com>
2021-04-27 16:22:28 +0300

    Update

--- webhook.py
+++ webhook.py
@@ -1,8 +1,7 @@
-import requests
+#import requests
 
 from flask import Flask
 from flask import request
-from flask import make_response
 from flask import jsonify
 
 
@@ -10,28 +9,28 @@ from flask import jsonify
 app = Flask(__name__)
 
 
-def makeResponse(dialogflow_data):
-    result = dialogflow_data.get('queryResult')
-    parameters = result.get('parameters')
-    city = parameters.get('geo-city')
-    date = parameters.get('date-time').split('T')[0]
+# def makeResponse(dialogflow_data):
+#     result = dialogflow_data.get('queryResult')
+#     parameters = result.get('parameters')
+#     city = parameters.get('geo-city')
+#     date = parameters.get('date-time').split('T')[0]
 
-    # Weather API
-    r = requests.get(f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid=2cbe6a057de3aadd1d2353cf765a1895")
-    json_object = r.json()
-    weather = json_object['list']
-    for i in range(len(weather)):
-        if date in weather[i]['dt_txt']:
-            condition = weather[i]['weather'][0]['description']
-            break
+#     # Weather API
+#     r = requests.get(f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid=2cbe6a057de3aadd1d2353cf765a1895")
+#     json_object = r.json()
+#     weather = json_object['list']
+#     for i in range(len(weather)):
+#         if date in weather[i]['dt_txt']:
+#             condition = weather[i]['weather'][0]['description']
+#             break
 
-    speech = f"The forecast for {city} on {date} is {condition}"
+#     speech = f"The forecast for {city} on {date} is {condition}"
 
-    return {'fulfillmentText': speech}
+#     return {'fulfillmentText': speech}
 
  
-@app.route('/webhook', methods=['POST'])
-def webhook():
+#@app.route('/webhook', methods=['POST'])
+#def webhook():
     #dialogflow_data = request.get_json(silent=True)
     #return jsonify(makeResponse(dialogflow_data))
 
